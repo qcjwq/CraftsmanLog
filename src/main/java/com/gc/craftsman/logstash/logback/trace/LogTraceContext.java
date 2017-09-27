a4f177713fde66d3b3a905a224858a29b09bd984
cjw <qcjwq@vip.qq.com>
2017-09-27 08:13:15 +0800

    Adjust dependent version

--- src/main/java/com/gc/craftsman/logstash/logback/trace/LogTraceContext.java
+++ src/main/java/com/gc/craftsman/logstash/logback/trace/LogTraceContext.java
@@ -3,8 +3,6 @@ package com.gc.craftsman.logstash.logback.trace;
 import com.gc.craftsman.logstash.logback.thread.RequestScopeContext;
 import net.logstash.logback.encoder.org.apache.commons.lang.StringUtils;
 
-import java.util.Map;
-
 /**
  * Created by cjw on 23/09/2017.
  */
